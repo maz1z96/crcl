79c5870c81f1d78e1b394b9d46be7ab6bf4f643b
maz1z96 <mark.aziz96@live.co.uk>
2026-01-27 17:37:29 +0000

    Exercise 1.1 - See ChatGPT

--- crcl.py
+++ crcl.py
@@ -1,22 +1,34 @@
 # Cockcroft-Gault CrCl, mL/min = (140 – age) × (weight, kg) × (1.23 for male or 1.04 for female) / (Cr, micromol/L)
+def main():
+    age = get_positive_number("Age", int)
+    weight = get_positive_number("Weight (kg)", float)
+    cr = get_positive_number("Serum Creatinine (micromol/L)", float)
+    sex = input("Sex (M/F) ").title()
 
-age = int(input("Age "))
-weight = float(input("Weight (kg) "))
-sex = input("Sex (M/F) ").title()
-cr = float(input("Serum Cr (micromol/L) "))
+    if sex == "M":
+        constant = 1.23
+    elif sex == "F":
+        constant = 1.04
 
-if sex == "M":
-    constant = 1.23
-elif sex == "F":
-    constant = 1.04
+    crcl = (140 - age) * weight * constant / cr
+    crcl = round(crcl, 2)
 
-crcl = (140 - age) * weight * constant / cr
-crcl = round(crcl, 2)
+    print()
+    print(f'CrCl = {crcl} ml/min')
 
-print()
-print(f'CrCl = {crcl} ml/min')
+def get_positive_number(prompt, type, min=0):
+    while True:
+        try:
+            number = type(input(f'{prompt}: '))
+        except ValueError:
+            print("Please enter a valid number")
+            print()
+        else:
+            if number < min:
+                print("Number cannot be negative. Please try again")
+                print()
+            else:
+                break
+    return number
 
-# Use cases
-## Sex is not M or F - is there a way to only select m or f - inquirer
-## Age, Weight, Cr are non-numbers
-## Add group of renal impairment - dictionary
+main()
