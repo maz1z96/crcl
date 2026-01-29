3c16dccf4e902da0245268adc34042269580b0b8
maz1z96 <mark.aziz96@live.co.uk>
2026-01-29 17:36:51 +0000

    Exercise 1.3a - renal function categories with ranges

--- crcl.py
+++ crcl.py
@@ -7,9 +7,11 @@ def main():
 
     crcl = (140 - age) * weight * constant / cr
     crcl = round(crcl, 2)
+    reading = renal_function(crcl)
 
     print()
     print(f'CrCl = {crcl} ml/min')
+    print(f'Renal function = {reading}')
 
 def get_parameter(prompt, type, min=0):
     while True:
@@ -40,8 +42,21 @@ def get_constant():
             print("Please enter either M or F")
             print()
 
+def renal_function(result):
+    categories = {
+        range(90, 5000): "Normal renal function",
+        range(60, 90): "Mild renal function",
+        range(45, 60): "Mild-moderate renal function",
+        range(30, 45): "Moderate-severe renal function",
+        range(15, 30): "Severe reduction",
+        range(0, 15): "Kidney Failure"
+    }
 
+    int_result = int(result)
 
+    for gfr in categories:
+        if int_result in gfr:
+            return categories[gfr]
 # Execution
 if __name__ == "__main__":
     main()
