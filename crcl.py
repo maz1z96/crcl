bf346b729d42fbdb484d84706cce8cbf2da01380
maz1z96 <mark.aziz96@live.co.uk>
2026-01-29 16:26:23 +0000

    Exercise 1.1: validated numeric inputs with type and positive checks

--- crcl.py
+++ crcl.py
@@ -1,8 +1,8 @@
 # Cockcroft-Gault CrCl, mL/min = (140 – age) × (weight, kg) × (1.23 for male or 1.04 for female) / (Cr, micromol/L)
 def main():
-    age = get_positive_number("Age", int)
-    weight = get_positive_number("Weight (kg)", float)
-    cr = get_positive_number("Serum Creatinine (micromol/L)", float)
+    age = get_parameter("Age", int)
+    weight = get_parameter("Weight (kg)", float)
+    cr = get_parameter("Serum Creatinine (micromol/L)", float)
     sex = input("Sex (M/F) ").title()
 
     if sex == "M":
@@ -16,7 +16,7 @@ def main():
     print()
     print(f'CrCl = {crcl} ml/min')
 
-def get_positive_number(prompt, type, min=0):
+def get_parameter(prompt, type, min=0):
     while True:
         try:
             number = type(input(f'{prompt}: '))
@@ -31,4 +31,5 @@ def get_positive_number(prompt, type, min=0):
                 break
     return number
 
-main()
+if __name__ == "__main__":
+    main()
